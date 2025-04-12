906cb3d8e9a1c1a7c70fcfb7eae5372d7d84956b
Superalex0102 <superalex0102@gmail.com>
2025-04-12 13:32:04 +0200

    added basic api usage

--- movie-app/ui/genre-section/GenreSectionView.swift
+++ movie-app/ui/genre-section/GenreSectionView.swift
@@ -10,13 +10,18 @@ import SwiftUI
 class GenreSectionViewModel: ObservableObject {
     @Published var genres: [Genre] = []
     
-    func loadGenres() {
-        self.genres = [
-            Genre(id: 1, name: "Adventure"),
-            Genre(id: 2, name: "Sci-fi"),
-            Genre(id: 3, name: "Fantasy"),
-            Genre(id: 4, name: "Comedy"),
-        ]
+    private var movieService : MoviesServiceProtocol = MoviesService()
+    
+    func fetchGenres() async {
+        do {
+            let request = FetchGenreRequest()
+            let genres = try await movieService.fetchGenres(req: request)
+            DispatchQueue.main.async {
+                self.genres = genres
+            }
+        } catch {
+            print("Error fetching genres: \(error)")
+        }
     }
 }
 
@@ -25,32 +30,44 @@ struct GenreSectionView: View {
     @StateObject private var viewModel = GenreSectionViewModel()
     
     var body: some View {
-        NavigationView {
-            List(viewModel.genres) { genre in
-                ZStack {
-                    NavigationLink(destination: Color.gray) {
-                        EmptyView()
-                    }
-                    .opacity(0)
-                    
-                    HStack {
-                        Text(genre.name)
-                            .font(Fonts.title)
-                            .foregroundStyle(Color.primary)
-                        Spacer()
-                        Image(.rightArrow)
-                    }
-                }
-                .listRowBackground(Color.clear)
-                .listRowSeparator(.hidden)
+        ZStack(alignment: .topTrailing) {
+            GeometryReader { geometry in
+                Image(.ellipse)
+                    .resizable()
+                    .frame(width: 449, height: 449)
+                    .position(x: geometry.size.width - 205, y: 225)
             }
-            .background(Color.clear)
-            .listStyle(.plain)
-            .navigationTitle("genreSection.title")
+            
+            NavigationView {
+                List(viewModel.genres) { genre in
+                    ZStack {
+                        NavigationLink(destination: Color.gray) {
+                            EmptyView()
+                        }
+                        .opacity(0)
+                        
+                        HStack {
+                            Text(genre.name)
+                                .font(Fonts.title)
+                                .foregroundStyle(Color.primary)
+                            Spacer()
+                            Image(.rightArrow)
+                        }
+                    }
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                }
+                .background(Color.clear)
+                .listStyle(.plain)
+                .navigationTitle(Environment.name == .dev ? "DEV" : "PROD")
+            }
+            .onAppear {
+                Task {
+                    await viewModel.fetchGenres()
+                }
+            }
         }
-        .onAppear {
-            viewModel.loadGenres()
-        }
+        .ignoresSafeArea()
     }
 }
 
