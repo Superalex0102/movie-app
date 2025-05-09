0db6dd720987edb05d583d39dfc2a4c931c8ee2c
Alex <41546701+Superalex0102@users.noreply.github.com>
2025-05-09 22:00:16 +0200

    Feature/ellipse (#5)
    * Added ellipse in the right top corner
    
    * Fixed ellipse

--- movie-app/ui/genre-section/GenreSectionView.swift
+++ movie-app/ui/genre-section/GenreSectionView.swift
@@ -14,30 +14,34 @@ struct GenreSectionView: View {
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
-            GeometryReader { geometry in
-                Image(.ellipse)
-                    .resizable()
-                    .frame(width: 449, height: 449)
-                    .position(x: geometry.size.width - 205, y: 225)
-            }
-            
             NavigationView {
-                List(viewModel.genres) { genre in
-                    ZStack {
-                        NavigationLink(destination: MovieListView(genre: genre)) {
-                            EmptyView()
-                        }
-                        .opacity(0)
-                        
-                        GenreSectionCell(genre: genre)
+                ZStack {
+                    GeometryReader { geometry in
+                        Image(.ellipse)
+                            .resizable()
+                            .frame(width: 400, height: 400)
+                            .position(x: geometry.size.width - 50, y: 50)
+                            .padding()
                     }
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
+                    .ignoresSafeArea()
+                    
+                    List(viewModel.genres) { genre in
+                        ZStack {
+                            NavigationLink(destination: MovieListView(genre: genre)) {
+                                EmptyView()
+                            }
+                            .opacity(0)
+                            
+                            GenreSectionCell(genre: genre)
+                        }
+                        .listRowBackground(Color.clear)
+                        .listRowSeparator(.hidden)
+                    }
+                    .background(Color.clear)
+                    .listStyle(.plain)
+                    .navigationTitle(Environment.name == .tv ? "TV" : "genreSection.title")
+                    .accessibilityLabel("testCollectionView")
                 }
-                .background(Color.clear)
-                .listStyle(.plain)
-                .navigationTitle(Environment.name == .tv ? "TV" : "genreSection.title")
-                .accessibilityLabel("testCollectionView")
             }
             .alert(item: $viewModel.alertModel) { model in
                 return Alert(
